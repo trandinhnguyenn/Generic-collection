9d0f9ec2c8381ce8b419e8fc260eee091ea94491
Tran Dinh Nguyen <dinhnguyentran00@gmail.com>
2026-03-17 14:23:19 +0700

    Bai5

---
+++ bai5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Tran Dinh Nguyen - MSSV: 2415053122225\n");
+        List<string> students = new List<string> { "An", "Binh", "Cuong", "Dung", "Hoa" };
+
+        Console.WriteLine("Danh sach hien tai: " + string.Join(", ", students));
+
+        Console.Write("Nhap ten sinh viên muon xoa: ");
+        string nameToRemove = Console.ReadLine();
+        if (students.Remove(nameToRemove))
+        {
+            Console.WriteLine($"Da xoa thanh cong ten '{nameToRemove}'!");
+        }
+        else
+        {
+            Console.WriteLine($"Khong tim thay ten '{nameToRemove}' trong danh sach.");
+        }
+        Console.WriteLine("\nDanh sach sau khi cap nhat: " + string.Join(", ", students));
+    }
+}
