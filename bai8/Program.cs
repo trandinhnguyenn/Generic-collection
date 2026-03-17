1ddc3e5b17e9edbb259b33701fd28216cb1a0931
Tran Dinh Nguyen <dinhnguyentran00@gmail.com>
2026-03-17 15:06:31 +0700

    Baí

---
+++ bai8/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Tran Dinh Nguyen - MSSV: 2415053122225\n");
+
+        List<string> words = new List<string> { "Mot", "Hai", "Ba", "Bon", "Nam" };
+
+        Console.WriteLine("Danh sach ban dau: " + string.Join(", ", words));
+        words.Reverse();
+
+        Console.WriteLine("Danh sach sau khi dao nguoc: " + string.Join(", ", words));
+    }
+}
